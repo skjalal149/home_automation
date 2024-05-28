9f0aab00bec3b25c0cab28dc739ae95b26fc2675
“Waseem <waseem@binarysoftech.co.in>
2024-05-28 21:10:39 +0530

    Admin Added

--- Device/admin.py
+++ Device/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ['room', 'name', 'deviceID']
--- Widget/admin.py
+++ Widget/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Button)
+class ButtonAdmin(admin.ModelAdmin):
+    list_display = ['device', 'name', 'button_type', 'status']
+
+
+@admin.register(ProgressBar)
+class ProgressBarAdmin(admin.ModelAdmin):
+    list_display = ['device', 'name', 'value']
+
+
+@admin.register(ColorStatus)
+class ColorStatusAdmin(admin.ModelAdmin):
+    list_display = ['device', 'name', 'value']
